b314420cb138fea3c9e9b9d526812a92cb01fef0
victorstassun <victorstassun@gmail.com>
2021-08-13 09:03:48 -0300

    Atualização dos cards

---
+++ src/pages/components/CardProject/CardEdit/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { FaEdit } from "react-icons/fa";
+
+import { Card, CardStatus, CardBox, BoxLeft, BoxRight } from './styles';
+
+interface CardStatus {
+    statusColor: string;
+}
+
+const CardProject: React.FC<CardStatus> = ({statusColor}) => {
+    return (
+        <>
+            <Card>
+                <CardStatus statusColor={statusColor}/>
+                <CardBox>
+                    <BoxLeft>
+                        <div>
+                            <p>1000025562 - Seção ABC</p>
+                            <h1>WEC - IMPLATAÇÃO DE EDI CLIENTE XYZ</h1>
+                        </div>
+                        <div>
+                            <p><strong>Saldo previsto:</strong> R$ 50.000,00</p>
+                            <p><strong>Saldo restante:</strong> R$ 50.000,00</p>
+                        </div>
+                    </BoxLeft>
+                    <BoxRight>
+                        <div>
+                            <FaEdit size={20}/>
+                        </div>
+                        <div>
+                            <p><strong>Horas:</strong> <AiOutlineClockCircle size={15} /> 120 Horas</p>
+                        </div>
+                    </BoxRight>
+                </CardBox>
+            </Card>
+        </>
+    );
+}
+
+export default CardProject;
---
+++ src/pages/components/CardProject/CardEdit/styles.ts
@@ -0,0 +1,172 @@
+import styled, { css } from "styled-components";
+
+interface StatusProps {
+    statusColor: string;
+}
+
+const statusTypeVariations = {
+    Atrasado: css `
+        background-color: red;
+    `,
+    NaoIniciado: css `
+        background-color: grey;
+    `,
+    EmAndamento: css `
+        background-color: #00579D;
+    `
+}
+
+export const Card = styled.div`
+    width: 36vw;
+    height: 20vh;
+
+    margin-top: 3vh;
+
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+`;
+
+export const CardStatus = styled.div<StatusProps>`
+    width: 1vw;
+    height: 100%;
+
+    border-radius: 5px 0px 0px 5px;
+    display: flex;
+    flex-direction: row;
+
+    ${props => props.statusColor === "Atrasado" && css`background-color: red;`}
+
+    ${props => props.statusColor === "NaoIniciado" && css`background-color: grey;`}
+
+    ${props => props.statusColor === "EmAndamento" && css`background-color: #00579D`}
+`;
+
+export const CardBox = styled.div`
+    width: 35vw;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    padding: 0.5vh;
+
+    border: 0.1vh solid rgb(0, 0, 0, 0.15);
+    border-left: 0;
+    border-radius: 0px 5px 5px 0px;
+    box-shadow: 2px 2px 3px rgb(0, 0, 0, 0.25);
+
+    transition: all 0.2s;
+
+    &:hover {
+        box-shadow: 4px 4px 6px rgb(0, 0, 0, 0.25);
+        cursor: pointer;
+    }
+`;
+
+export const BoxLeft = styled.div`
+    width: 24vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    div {
+        display: flex;
+        flex-direction: column;
+        padding: 5px;
+
+        strong { 
+            color: #222;
+            margin-right: 1vw;
+        }
+
+        p {
+            font-size: 2vh;
+            padding: 2px;
+            padding-left: 10px;
+            text-shadow: 1px 1px 2px rgb(0, 0, 0, 0.2);
+            color: #5B5B5B;
+
+            strong {
+                font-weight: 500;
+                color: #000
+            }
+        }
+
+        h1 {
+            font-size: 2.1vh;
+            font-weight: bold;
+            padding-top: 2px;
+            padding-left: 12px;
+        }
+
+        &:first-child p:first-child {
+            color: #229FC6;
+        }
+
+        &:last-child {
+            flex-direction: column;
+        }
+        
+        &:last-child p {
+            color: #787676;
+            margin-bottom: 0.4vh;
+        } 
+    }
+`;
+
+export const BoxRight = styled.div`
+    width: 13vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    
+    div {
+        display: flex;
+        
+        justify-content: center;
+        padding: 5px;
+
+        &:first-child {
+            flex-direction: row-reverse;
+            justify-content: space-between;
+        }
+
+        svg {
+            color: #00579D;
+            display: flex;
+        }
+
+        p {
+            font-size: 2vh;           
+            text-shadow: 1px 1px 2px rgb(0, 0, 0, 0.2);
+            color: #5B5B5B;
+
+            display: flex;
+            align-items: center;
+
+            strong { 
+                color: #000;
+                font-weight: 500;
+                margin-right: 0.2vw;
+            }
+
+            svg {
+                color: #00579D;
+                margin-right: 0.2vw;
+            }
+        }
+
+        &:first-child strong {
+            margin-left: 0.4vw;
+        }
+
+        &:last-child p:last-child {
+            padding-top: 1.7vh;
+            padding-bottom: 0.8vh;
+            text-align: left;
+        }
+        
+        &:last-child p {
+            color: #5B5B5B;
+        }
+    }
+`;
