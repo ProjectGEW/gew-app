c2421e91750cdf3396dcb944e170d9dc64198a13
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-08-15 20:39:26 -0300

    Adição do dropdown de bandeira

--- src/pages/components/Navbar/index.tsx
+++ src/pages/components/Navbar/index.tsx
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 
-import { ContainerNavbar, Logo, PageIndicator, SandwichMenu, DropdownMenu,
-    LanguageIndicator, Flag, VectorLanguage } from './styles';
+import { IoIosArrowDown } from "react-icons/io";
+
+import { ContainerNavbar, Logo, PageIndicator, SandwichMenu, DropdownMenu, LanguageIndicator, Flag, DropdownFlag } from './styles';
 
 
 const Navbar: React.FC = () => {
 
-    function openMenuNavbar() {
+    function openDropDown() {
         var elemento = document.getElementById("dropdownMenu")!.style.display;
-        
+
         if(elemento === 'block') {
             document.getElementById("dropdownMenu")!.style.display = 'none';
         } else {
             document.getElementById("dropdownMenu")!.style.display = 'block';
+            document.getElementById("dropdownFlag")!.style.display = 'none';
+        }
+    }
+
+    function openMenuFlag() {
+        var elemento = document.getElementById("dropdownFlag")!.style.display;
+        
+        if(elemento === 'block') {
+            document.getElementById("dropdownFlag")!.style.display = 'none';
+        } else {
+            document.getElementById("dropdownFlag")!.style.display = 'block';
+            document.getElementById("dropdownMenu")!.style.display = 'none';
         }
     }
 
     return (
-
         <ContainerNavbar>
                 <Logo></Logo>
                 <PageIndicator>
                     <a href="./">Menu</a>
                 </PageIndicator>
-                <SandwichMenu onClick={openMenuNavbar}>
+                <SandwichMenu onClick={openDropDown}>
                     <DropdownMenu id="dropdownMenu">
                         <ul>
                             <li><a href="./">Perfil</a></li>
@@ -34,10 +46,17 @@ const Navbar: React.FC = () => {
                 </SandwichMenu>
                 <LanguageIndicator>
                     <Flag></Flag>
-                    <VectorLanguage></VectorLanguage>
+                    <IoIosArrowDown size={20} onClick={openMenuFlag}/>
                 </LanguageIndicator>
+                <DropdownFlag id="dropdownFlag">
+                    <ul>
+                        <li><a href="./">ES</a></li>
+                        <li><a href="./">EN</a></li>
+                        <li><a href="./">UK</a></li>
+                        <li><a href="./">FR</a></li>
+                    </ul>
+                </DropdownFlag>
             </ContainerNavbar>
-
     );
 };
 
