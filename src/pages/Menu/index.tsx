8ed7488fb3170c7c93733062c11794731be79ff2
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-08-19 20:37:52 -0300

    Adição da configuração do idioma

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -7,6 +7,8 @@ import { CgInsertAfterR } from 'react-icons/cg';
 import { GoGraph } from 'react-icons/go';
 import { GiOrganigram } from 'react-icons/gi';
 
+import intl from 'react-intl-universal';
+
 import MenuLeft from '../components/MenuLeft';
 import Navbar from '../components/Navbar';
 
@@ -14,21 +16,32 @@ import { ContainerMenuRight, ContIcons, Icon, TextMenuRight } from './styleMenuR
 
 import { ContainerHome, ContainerHomeGraph, Card, ContainerHomeCards, ContainerHomeTitle, Graph, GraphTitle,
     CardContent, GraphContainer, GraphCont, GraphContNum, GraphBars, Bar, GraphData, Data } from './styles';
-       
+
+const locales = {
+    'pt-BR': require('../../language/pt-BR.json'),
+    'en-US': require('../../language/en-US.json'),
+};
+
 const Menu: React.FC = () => {
+
+    intl.init({
+        currentLocale: 'pt-BR',
+        locales
+      });
+
     return (
         <>
             <Navbar />
             <MenuLeft />
             <ContainerHome>
                 <ContainerHomeTitle>
-                    <h1><strong>Visão</strong> Geral</h1>
+                    <h1><strong>{intl.get('head.titulo_negrito')}</strong> {intl.get('head.titulo_normal')}</h1>
                     <span />
                 </ContainerHomeTitle>
                 <ContainerHomeCards>
                     <Card>
                         <div>
-                            <h2>Projetos Concluídos:</h2>
+                            <h2>{intl.get('cards.primeiro.titulo')}</h2>
                         </div>
                         <CardContent>
                             <span />
@@ -36,12 +49,12 @@ const Menu: React.FC = () => {
                             <GraphContainer />
                         </CardContent>
                         <div>
-                            <p><strong>Verba Total:</strong>  R$ 159.956,76 <AiFillEye id="icon-eye"/></p>
+                            <p><strong>{intl.get('cards.verba')}</strong>  R$ 159.956,76 <AiFillEye id="icon-eye"/></p>
                         </div>
                     </Card>
                     <Card>
                         <div>
-                            <h2>Projetos Em Andamento:</h2>
+                            <h2>{intl.get('cards.segundo.titulo')}</h2>
                         </div>
                         <CardContent>
                             <span />
@@ -49,12 +62,12 @@ const Menu: React.FC = () => {
                             <GraphContainer />
                         </CardContent>
                         <div>
-                            <p><strong>Verba Total:</strong>  R$ 159.956,76 <AiFillEye id="icon-eye"/></p>
+                            <p><strong>{intl.get('cards.verba')}</strong>  R$ 159.956,76 <AiFillEye id="icon-eye"/></p>
                         </div>
                     </Card>
                     <Card>
                         <div>
-                            <h2>Projetos Atrasados:</h2>
+                            <h2>{intl.get('cards.terceiro.titulo')}</h2>
                         </div>
                         <CardContent>
                             <span />
@@ -62,13 +75,13 @@ const Menu: React.FC = () => {
                             <GraphContainer />
                         </CardContent>
                         <div>
-                            <p><strong>Verba Total:</strong>  R$ 159.956,76 <AiFillEye id="icon-eye"/></p>
+                            <p><strong>{intl.get('cards.verba')}</strong>  R$ 159.956,76 <AiFillEye id="icon-eye"/></p>
                         </div>
                     </Card>
                 </ContainerHomeCards>
                 <ContainerHomeGraph>
                     <GraphTitle>
-                        <h1>PROJETOS TERMINADOS NOS ÚLTIMOS 7 DIAS</h1>
+                        <h1>{intl.get('grafico.titulo')}</h1>
                     </GraphTitle>
                     <Graph>
                         <GraphCont>
@@ -80,13 +93,13 @@ const Menu: React.FC = () => {
                             <GraphContNum>0</GraphContNum>
                         </GraphCont>
                         <GraphBars>
-                            <Bar id="bar1"></Bar>
-                            <Bar id="bar2"></Bar>
-                            <Bar id="bar3"></Bar>
-                            <Bar id="bar4"></Bar>
-                            <Bar id="bar5"></Bar>
-                            <Bar id="bar6"></Bar>
-                            <Bar id="bar7"></Bar>
+                            <Bar id="bar1"/>
+                            <Bar id="bar2"/>
+                            <Bar id="bar3"/>
+                            <Bar id="bar4"/>
+                            <Bar id="bar5"/>
+                            <Bar id="bar6"/>
+                            <Bar id="bar7"/>
                         </GraphBars>
                     </Graph>
                     <GraphData>
@@ -106,8 +119,8 @@ const Menu: React.FC = () => {
                         <AiOutlineFolderView size={50} color="#fff" /> 
                     </Icon> 
                     <TextMenuRight>
-                        <a href="../Projects/">VISUALIZAR</a>
-                        <a href="../Projects/">PROJETOS</a>
+                        <a href="../Projects/">{intl.get('menu_direito.primeiro_botao.primeiro')}</a>
+                        <a href="../Projects/">{intl.get('menu_direito.primeiro_botao.segundo')}</a>
                     </TextMenuRight>
                 </ContIcons>
                 <ContIcons>
@@ -115,8 +128,8 @@ const Menu: React.FC = () => {
                         <CgInsertAfterR size={50} color="#fff" />
                     </Icon>
                     <TextMenuRight>
-                        <a href="./">CADASTRAR</a>
-                        <a href="./">PROJETOS</a>
+                        <a href="./RegisterProjects">{intl.get('menu_direito.segundo_botao.primeiro')}</a>
+                        <a href="./RegisterProjects">{intl.get('menu_direito.segundo_botao.segundo')}</a>
                     </TextMenuRight>
                 </ContIcons>
                 <ContIcons>
@@ -124,8 +137,8 @@ const Menu: React.FC = () => {
                         <RiFileEditFill size={50} color="#fff" />
                     </Icon>
                     <TextMenuRight>
-                        <a href="./">EDITAR</a>
-                        <a href="./">PROJETOS</a>
+                        <a href="./EditProjects">{intl.get('menu_direito.terceiro_botao.primeiro')}</a>
+                        <a href="./EditProjects">{intl.get('menu_direito.terceiro_botao.segundo')}</a>
                     </TextMenuRight>
                 </ContIcons>
                 <ContIcons>
@@ -133,8 +146,8 @@ const Menu: React.FC = () => {
                         <IoPersonAddSharp size={50} color="#fff" />
                     </Icon>
                     <TextMenuRight>
-                        <a href="./">CADASTRAR</a>
-                        <a href="./">CONSULTORES</a>
+                        <a href="./">{intl.get('menu_direito.quarto_botao.primeiro')}</a>
+                        <a href="./">{intl.get('menu_direito.quarto_botao.segundo')}</a>
                     </TextMenuRight>
                 </ContIcons>
                 <ContIcons>
@@ -142,8 +155,8 @@ const Menu: React.FC = () => {
                         <GiOrganigram size={50} color="#fff" />
                     </Icon>
                     <TextMenuRight>
-                        <a href="./">ALOCAR</a>
-                        <a href="./">CONSULTORES</a>
+                        <a href="./">{intl.get('menu_direito.quinto_botao.primeiro')}</a>
+                        <a href="./">{intl.get('menu_direito.quinto_botao.segundo')}</a>
                     </TextMenuRight>
                 </ContIcons>
                 <ContIcons>
@@ -151,8 +164,8 @@ const Menu: React.FC = () => {
                         <GoGraph size={50} color="#fff" />
                     </Icon>
                     <TextMenuRight>
-                        <a href="./">IR PARA</a>
-                        <a href="./">DASHBOARDS</a>
+                        <a href="./Dashboard">{intl.get('menu_direito.sexto_botao.primeiro')}</a>
+                        <a href="./Dashboard">{intl.get('menu_direito.sexto_botao.segundo')}</a>
                     </TextMenuRight>
                 </ContIcons>
         </ContainerMenuRight>
