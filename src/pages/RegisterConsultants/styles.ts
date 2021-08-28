64ecd6d82be849690c5e2d7fa560fe68769b6660
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-08-28 13:08:06 -0300

    Início do layout

--- src/pages/RegisterConsultants/styles.ts
+++ src/pages/RegisterConsultants/styles.ts
@@ -1,1 +1,18 @@
 import styled from 'styled-components';
+
+export const Container = styled.div`
+    position: absolute;
+    top: 14vh;
+    left: 12vw;
+    width: 62vw;
+    height: 80vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    background-color: #fff;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+    border-radius: 0.8vh;
+`;
