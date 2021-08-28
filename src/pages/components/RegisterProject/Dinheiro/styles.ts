aaf1b73aba2fabf74ce55bd84f0c5682ae01ca0c
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-08-28 13:05:41 -0300

    Atualização da página para cadastrar projeto

--- src/pages/components/RegisterProject/Dinheiro/styles.ts
+++ src/pages/components/RegisterProject/Dinheiro/styles.ts
@@ -14,8 +14,6 @@ export const Box = styled.div`
 
 export const Table = styled.div`
     width: 50vw;
-    padding: 1.5vh;
-
     margin-bottom: 4vh;
 
     display: flex;
@@ -23,12 +21,38 @@ export const Table = styled.div`
     justify-content: center;
     flex-direction: column;
 
-    background-image: linear-gradient(to bottom, #00579D 35%, #fff 35%, #fff 100%);
+    overflow: hidden;
+
+    background-color: #fff;
     border-radius: 0.8vh;
     box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
 
+    #first-table, #second-table {
+        background-color: #00579D;
+    }
+
+    #first-scroll, #second-scroll {
+        max-height: 18vh;
+
+        flex-direction: column;
+        overflow: scroll;
+        overflow-x: hidden;
+
+        ::-webkit-scrollbar {
+            background-color: rgb(196, 196, 196, 0.5);
+            width: 0.5vw;
+        }
+        
+        ::-webkit-scrollbar-thumb {
+            background-color: rgb(196, 196, 196); 
+            border-radius: 1vh;
+        }
+    }
+
     div {
         width: 100%;
+        padding: 1.5vh;
+
         display: flex;
         justify-content: space-between;
         flex-direction: row;
@@ -46,28 +70,28 @@ export const Table = styled.div`
 
             h1 {
                 margin-left: 1.6vw;
-                margin-right: 1.6vw;
+                margin-right: 1.7vw;
             }
 
             &:last-child {
                 input {
                     &:nth-child(1) {
-                        width: 13.5vw;
+                        width: 12vw;
                         margin-left: 0vw;
                     }
             
                     &:nth-child(2) {
-                        width: 15.5vw;
+                        width: 16vw;
                         margin-left: 0vw;
                     }
             
                     &:nth-child(3) {
-                        width: 9vw;
+                        width: 6vw;
                         margin-right: 0vw;
                     }
     
                     &:nth-child(4) {
-                        width: 8.5vw;
+                        width: 10vw;
                         margin-right: 0vw;
                     }
                 }
@@ -79,8 +103,7 @@ export const Table = styled.div`
 
 export const Linha = styled.div`
     width: 100%;
-    margin-top: 4vh;
-    margin-bottom: 1vh;
+    transition: all 0.2s;
 
     input {
         height: 5vh;
@@ -104,21 +127,20 @@ export const Linha = styled.div`
         &:nth-child(3) {
             width: 12vw;
         }
-
-        &:last-child {
-            &::after {
-                content: '';
-                width: 4vw;
-                height: 4vh;
-                position: absolute;
-                
-                margin-top: 0vh;
-                margin-left: 0vw;
-
-                background-image: url(${Plus});
-                background-size: cover;
-                background-position: center;
-            }
-        }
     }
+
+    /*&::before {
+        content: '';
+        width: 1.2vw;
+        height: 3vh;
+        position: absolute;
+        
+        margin-top: 1vh;
+        margin-left: 50vw;
+
+        background-image: url(${Plus});
+        background-size: cover;
+        background-position: center;
+        cursor: pointer;
+    }*/
 `;
