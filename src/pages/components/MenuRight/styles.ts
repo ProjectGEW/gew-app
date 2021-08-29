b55c1cf4b3157e9cae76ae0035c862b95e2c1937
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-08-29 13:30:16 -0300

    ~Menu de navegação finalizado

--- src/pages/components/MenuRight/styles.ts
+++ src/pages/components/MenuRight/styles.ts
@@ -1,160 +1,129 @@
 import styled, { css } from "styled-components";
 
-/*interface TypeRoute {
-    type?: 'Dashboard' | 'EditProjects' | 'RegisterConsultants' | 'RegisterProjects' | 'Projects';
-}*/
-
-/*const styleTypes = {
-    Dashboard: css `
-        &:nth-child(6) {
-            margin-left: -0.7vw;
-
-            #icons {
-                margin-left: 1.1vw;
-            }
-            
-            &:before {
-                content: "";
-                width: 0.1vw;
-                height: 10vh;
-                margin-left: 0.8vw;
-                background: #fff;
-                position: absolute;
-            }
-        }
-    `,
-    EditProjects: css `
-        &:nth-child(3) {
-            margin-left: -0.7vw;
-
-            #icons {
-                margin-left: 1.1vw;
-            }
-            
-            &:before {
-                content: "";
-                width: 0.1vw;
-                height: 10vh;
-                margin-left: 0.8vw;
-                background: #fff;
-                position: absolute;
-            }
-        }
-    `,
-    RegisterConsultants: css `
-        &:nth-child(4) {
-            margin-left: -0.7vw;
-
-            #icons {
-                margin-left: 1.1vw;
-            }
-            
-            &:before {
-                content: "";
-                width: 0.1vw;
-                height: 10vh;
-                margin-left: 0.8vw;
-                background: #fff;
-                position: absolute;
-            }
-        }
-    `,
-    RegisterProjects: css `
-        &:nth-child(2) {
-            margin-left: -0.7vw;
-
-            #icons {
-                margin-left: 1.1vw;
-            }
-            
-            &:before {
-                content: "";
-                width: 0.1vw;
-                height: 10vh;
-                margin-left: 0.8vw;
-                background: #fff;
-                position: absolute;
-            }
-        }    
-    `,
-    Projects: css `
-        &:nth-child(1) {
-            margin-left: -0.7vw;
-
-            #icons {
-                margin-left: 1.1vw;
-            }
-            
-            &:before {
-                content: "";
-                width: 0.1vw;
-                height: 10vh;
-                margin-left: 0.8vw;
-                background: #fff;
-                position: absolute;
-            }
-        }    
-    `,
-};*/
-
 interface HistoricPageProps {
     localDaRota?: string;
   }
 
-export const ContainerMenuRight = styled.div`
+export const ContainerMenuRight = styled.div<HistoricPageProps>`
     width: 30vw;
     height: 76vh;
     margin: 9vh 0 0 97.7vw;
     display: flex;
     flex-direction: column;
     position: fixed;
-`;
 
-export const ContIcons = styled.div<HistoricPageProps>`
-    width: 100%;
-    height: 12.3vh;
-    margin-bottom: 0.4vh;
-    background-color: #0091BD;
-    display: flex;
-    justify-content: left;
-    align-items: center;
-    cursor: pointer;
-    transition: all 0.1s;
-
-    &:nth-child(1) { background-color: #00579D; }
-
-    &:nth-child(2) { background: linear-gradient(to bottom, #00579D, #0075B1); }
-
-    &:nth-child(3) { background-color: #0075B1; }
-
-    &:nth-child(4) { background: linear-gradient(to bottom, #0075B1, #0091BD); }
-
-    &:nth-child(1) { border-radius: 0.5vh 0 0 0; }
-
-    &:nth-child(6) { 
-        border-radius: 0 0 0 0.5vh; 
-        background: linear-gradient(to bottom, #0091BD, #009ecf); 
+    #first { 
+        background-color: #00579D;
+        &:hover {
+            transform: translateX(-30vh);
+            opacity: 0.9;
+    
+            &:before {
+                display: none;
+            }
+    
+            #icons {
+                transform: scale(3.2, 3.2);
+                margin-left: 5vh;
+            }
+        }
     }
 
-    &:hover {
-        transform: translateX(-30vh);
-        opacity: 0.9;
-
-        &:before {
-            display: none;
+    #second { 
+        background: linear-gradient(to bottom, #00579D, #0075B1); 
+        &:hover {
+            transform: translateX(-30vh);
+            opacity: 0.9;
+    
+            &:before {
+                display: none;
+            }
+    
+            #icons {
+                transform: scale(3.2, 3.2);
+                margin-left: 5vh;
+            }
         }
+    }
 
-        #icons {
-            transform: scale(3.2, 3.2);
-            margin-left: 5vh;
+    #three { 
+        background-color: #0075B1;
+        &:hover {
+            transform: translateX(-30vh);
+            opacity: 0.9;
+    
+            &:before {
+                display: none;
+            }
+    
+            #icons {
+                transform: scale(3.2, 3.2);
+                margin-left: 5vh;
+            }
         }
+    }
+
+    #four { 
+        background: linear-gradient(to bottom, #0075B1, #0091BD); 
+        &:hover {
+            transform: translateX(-30vh);
+            opacity: 0.9;
+    
+            &:before {
+                display: none;
+            }
+    
+            #icons {
+                transform: scale(3.2, 3.2);
+                margin-left: 5vh;
+            }
+        }
+    }
+
+    #five { 
+        border-radius: 0.5vh 0 0 0;
+        &:hover {
+            transform: translateX(-30vh);
+            opacity: 0.9;
+    
+            &:before {
+                display: none;
+            }
+    
+            #icons {
+                transform: scale(3.2, 3.2);
+                margin-left: 5vh;
+            }
+        }
+    }
+
+    #six { 
+        border-radius: 0 0 0 0.5vh; 
+        background: linear-gradient(to bottom, #0091BD, #009ecf); 
+        &:hover {
+            transform: translateX(-30vh);
+            opacity: 0.9;
+    
+            &:before {
+                display: none;
+            }
+    
+            #icons {
+                transform: scale(3.2, 3.2);
+                margin-left: 5vh;
+            }
+        }
+    } 
+
+    a {
+        transition: all 0.2s;
     }
 
     ${props => props.localDaRota === "/projects" && css`
-        &:nth-child(1) { 
+        #first { 
             transform: translateX(-3vh);
             border-left: 1vh solid white;
-            box-shadow: -0.1vh 0 0.1vh 0 rgba(50, 50, 93, 0.25);
+            box-shadow: -0.1vh 0 0.1vh 0 rgba(50, 50, 93, 0.45);
 
             #icons {
                 transform: scale(1.6, 1.6);
@@ -178,10 +147,10 @@ export const ContIcons = styled.div<HistoricPageProps>`
     `}
 
     ${props => props.localDaRota === "/register_projects" && css`
-        &:nth-child(2) { 
+        #second { 
             transform: translateX(-3vh);
             border-left: 1vh solid white;
-            box-shadow: -0.1vh 0 0.1vh 0 rgba(50, 50, 93, 0.25);
+            box-shadow: -0.1vh 0 0.1vh 0 rgba(50, 50, 93, 0.45);
 
             #icons {
                 transform: scale(1.6, 1.6);
@@ -205,10 +174,10 @@ export const ContIcons = styled.div<HistoricPageProps>`
     `}
 
     ${props => props.localDaRota === "/edit_projects" && css`
-        &:nth-child(3) { 
+        #three { 
             transform: translateX(-3vh);
             border-left: 1vh solid white;
-            box-shadow: -0.1vh 0 0.1vh 0 rgba(50, 50, 93, 0.25);
+            box-shadow: -0.1vh 0 0.1vh 0 rgba(50, 50, 93, 0.45);
 
             #icons {
                 transform: scale(1.6, 1.6);
@@ -232,10 +201,10 @@ export const ContIcons = styled.div<HistoricPageProps>`
     `}
 
     ${props => props.localDaRota === "/register_consultants" && css`
-        &:nth-child(4) { 
+        #four { 
             transform: translateX(-3vh);
             border-left: 1vh solid white;
-            box-shadow: -0.1vh 0 0.1vh 0 rgba(50, 50, 93, 0.25);
+            box-shadow: -0.1vh 0 0.1vh 0 rgba(50, 50, 93, 0.45);
 
             #icons {
                 transform: scale(1.6, 1.6);
@@ -259,10 +228,10 @@ export const ContIcons = styled.div<HistoricPageProps>`
     `}
 
     ${props => props.localDaRota === "/dashboard" && css`
-        &:nth-child(6) { 
+        #six { 
             transform: translateX(-3vh);
             border-left: 1vh solid white;
-            box-shadow: -0.1vh 0 0.1vh 0 rgba(50, 50, 93, 0.25);
+            box-shadow: -0.1vh 0 0.1vh 0 rgba(50, 50, 93, 0.45);
 
             #icons {
                 transform: scale(1.6, 1.6);
@@ -284,8 +253,18 @@ export const ContIcons = styled.div<HistoricPageProps>`
             }
         }
     `}
+`;
 
-    
+export const ContIcons = styled.div`
+    width: 100%;
+    height: 12.3vh;
+    margin-bottom: 0.4vh;
+    background-color: #0091BD;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    cursor: pointer;
+    transition: all 0.1s;  
 `;
 
 export const Icon = styled.div`
@@ -304,7 +283,7 @@ export const TextMenuRight = styled.div`
     flex-direction: column;
     align-items: center;
 
-    a {
+    p {
         color: #fff;
         font-size: 2.5vh;
         font-weight: 600;
