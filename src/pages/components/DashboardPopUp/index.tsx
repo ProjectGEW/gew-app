62e676252d246d97a902a07294fa72cfb2b73583
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-08-29 18:26:24 -0300

    PopUp do dashboard

---
+++ src/pages/components/DashboardPopUp/index.tsx
@@ -0,0 +1,75 @@
+import React, {useState, useEffect} from 'react';
+import Modal from '../CardPopUp/Modal';
+import Button from '../Button';
+import api from '../../../service/api'
+
+import { Container, PopUp, Title, Graph, Scroll, Bar, Value } from './styles';
+
+interface BaseModalWrapperProps {
+    isModalVisible: boolean;
+    onBackdropClick: () => void;
+}
+
+const BaseModalWrapper: React.FC<BaseModalWrapperProps> = ({onBackdropClick, isModalVisible}) => {
+
+    if (!isModalVisible) {
+        return null
+    }
+    
+    return (
+        <Modal onBackdropClick={onBackdropClick}>
+            <Container>
+                <PopUp>
+                    <Title>
+                        <h1>PROJETOS</h1>
+                        <span onClick={onBackdropClick} />
+                    </Title>
+                    <Scroll>
+                        <div className="projeto">
+                            <p>NOMEPROJETO 1</p>
+                            <p>R$ 10.312,21</p>
+                            <p>25%</p>
+                        </div>
+                        <div className="projeto">
+                            <p>NOMEPROJETO 2 </p>
+                            <p>R$ 2.612,21</p>
+                            <p>10%</p>
+                        </div>
+                        <div className="projeto">
+                            <p>NOMEPROJETO 3</p>
+                            <p>R$ 40.762,21</p>
+                            <p>11%</p>
+                        </div>
+                        <div className="projeto">
+                            <p>NOMEPROJETO 4</p>
+                            <p>R$ 4.243,21</p>
+                            <p>43%</p>
+                        </div>
+                        <div className="projeto">
+                            <p>NOMEPROJETO 5</p>
+                            <p>R$ 20.235,21</p>
+                            <p>12%</p>
+                        </div>
+                        <div className="projeto">
+                            <p>NOMEPROJETO 6</p>
+                            <p>R$ 50.212,21</p>
+                            <p>12%</p>
+                        </div>
+                        <div className="projeto">
+                            <p>NOMEPROJETO 7</p>
+                            <p>R$ 42.856,21</p>
+                            <p>16%</p>
+                        </div>
+                    </Scroll>
+                    <Graph>
+                        <Bar>
+                            <Value></Value>
+                        </Bar>
+                    </Graph>
+                </PopUp>
+            </Container>
+        </Modal>
+    );
+}
+
+export default BaseModalWrapper;
---
+++ src/pages/components/DashboardPopUp/styles.ts
@@ -0,0 +1,176 @@
+import styled from "styled-components";
+
+import Arrow from '../../../assets/arrow_grey.svg';
+import Close from '../../../assets/close.svg';
+
+export const Container = styled.div`
+    position: absolute;
+    top: 14vh;
+    left: 12vw;
+    width: 78vw;
+    height: 80vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const PopUp = styled.div`
+    width: 35vw;
+    height: 40vh;
+    background: #fff;
+    border-radius: 0.8vh;
+    overflow: hidden;
+
+    display: flex;
+    flex-direction: column;
+
+    .projeto {       
+        margin-top: 0vh;         
+        margin-left: 1vw;
+        margin-right: 1vw;
+
+        font-size: 2.5vh;
+        color: #00579D;
+
+        border-bottom: 0.1vh solid #ccc;
+
+        &:hover {
+            background-color: rgba(220, 220, 220, 0.25);
+        }
+
+        p {
+            &:nth-child(1) {
+                color: #484848;
+
+                &::after {
+                    content: '';
+                    width: 0.2vh;
+                    height: 3vh;
+                    margin-left: 3vw;
+                    position: absolute;
+                    //background-color: #ccc;
+                }
+            }
+
+            &:nth-child(2) {
+                &::after {
+                    content: '';
+                    width: 2vw;
+                    height: 2vh;
+                    margin-top: 0.4vh;
+                    margin-left: 2vw;
+                    position: absolute;
+                    //background-image: url(${Arrow});
+                    background-position: center;
+                    background-size: cover;
+                }
+            }
+        }
+
+        font-weight: bold;
+        padding: 1.4vh;
+        padding-top: 2vh;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+`;
+
+export const Scroll = styled.div`
+    width: 35vw;
+    height: 25vh;
+
+    display: flex;
+    flex-direction: column;
+
+    overflow: scroll;
+    overflow-x: hidden;
+
+    ::-webkit-scrollbar {
+        background-color: rgb(196, 196, 196, 0.5);
+        width: 0.5vw;
+    }
+       
+    ::-webkit-scrollbar-thumb {
+        background-color: rgb(196, 196, 196); 
+        border-radius: 1vh;
+    }
+`;
+
+
+export const Title = styled.div`
+    width: 35vw;
+    padding: 1vh;
+    background: #00579D;
+
+    display: flex;
+    flex-direction: row;
+
+    h1 {
+        color: #fff;
+        font-size: 3vh;
+    }
+
+    span {
+        background-image: url(${Close});
+        border: 0;
+        background-color: transparent;
+        width: 4vh;
+        height: 4vh;
+        margin-left: 32vw;
+        position: absolute;
+        background-size: cover;
+        cursor: pointer;
+
+        &:hover {
+            opacity: 0.9;
+        }
+
+        &:active {
+            opacity: 0.8;
+        }
+    }
+`;
+
+export const Graph = styled.div`
+    width: 35vw;
+    height: 10vh;
+ 
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Bar = styled.div`
+    width: 32vw;
+    height: 6vh;
+    background: white;
+    box-shadow: 0 0 0.1vh 0.1vh #aaa;
+
+    display: flex;
+    flex-direction: row;
+`;
+
+export const Value = styled.div`
+    width: 15vw;
+    height: 6vh;
+    background: linear-gradient(to right, #006FC8, #0090C5, #28B9DA);
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+    &::after {
+        content: '47%';
+        width: 2vw;
+        height: 2vh;
+        margin-top: 0.8vh;
+        margin-left: 42vw;
+        position: absolute;
+        color: #00579D;
+        font-weight: bold;
+        font-size: 4vh;
+    }
+`;
