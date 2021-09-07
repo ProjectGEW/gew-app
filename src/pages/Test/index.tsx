7f262839181d3a41aeac82e683dd1100d4f047e0
Cauã Kath <cauakathdev@gmail.com>
2021-09-07 20:22:42 -0300

    Upload da ata

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import MenuLeft from '../components/MenuLeft';
 import Navbar from '../components/Navbar';
@@ -7,10 +7,44 @@ import MenuRight from '../components/MenuRight';
 import { ContIcons } from '../components/MenuRight/styles';
 
 import { Container } from './styles';
+import Paper from "@material-ui/core/Paper";
+import RootRef from '@material-ui/core/RootRef';
+import { useDropzone } from "react-dropzone";
 
 import api from "../../service/api";
 
 const Projects: React.FC = () => {
+    const [file, setFile] = useState<Blob>();
+    const [downloadUri, setDownloadUri] = useState();
+    const [fileName, setFileName] = useState<string>();
+
+    const onDrop = useCallback((acceptedFiles) => {
+        console.log(acceptedFiles);
+        setFile(acceptedFiles[0]);
+        setFileName(acceptedFiles[0].name);
+    }, [])
+
+    const {getRootProps, getInputProps} = useDropzone({
+        multiple: false, 
+        onDropAccepted: onDrop,
+    });
+
+    const {ref, ...rootProps} = getRootProps();
+
+    const uploadFile = async () => {
+        try {
+            const formData = new FormData();
+
+            formData.append("file", file ? file : "");
+
+            const response = await api.post(`files/1`, formData);
+
+            setDownloadUri(response.data.fileDownloadUri);
+        } catch(err) {
+            alert(err.message);
+        }
+    }
+
     async function apontarHoras() {
         const input = (document.getElementById("horas") as HTMLInputElement).value;
 
@@ -29,13 +63,14 @@ const Projects: React.FC = () => {
             <input type="number" id="horas" />
             <button onClick={apontarHoras}>Enviar</button>
 
-            <div>
-                <label htmlFor="ata1">ATA 1</label>
-                <input type="file" id="ata1"/>
-
-                <label htmlFor="ata2">ATA 2</label>
-                <input type="file" id="ata2"/>
-            </div>
+            <RootRef rootRef={ref}>
+                <Paper {...rootProps}>
+                    <label htmlFor="ata1">UPLOAD</label>
+                    <input {...getInputProps()}/>
+                </Paper>
+                <button onClick={uploadFile}>Enviar</button>
+                
+            </RootRef>
         </Container>
 
         <MenuRight>
