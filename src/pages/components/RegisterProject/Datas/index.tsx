f82fe6fe50df1a57ccf929658e0826076d62a674
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-09-07 18:08:55 -0300

    Atualização da tela

--- src/pages/components/RegisterProject/Datas/index.tsx
+++ src/pages/components/RegisterProject/Datas/index.tsx
@@ -8,21 +8,23 @@ const Datas: React.FC = () => {
     return (
         <>
         <Box>
-            <div>
-                <label>Data de ínicio:</label>
-                <label>Data de término:</label>
-                <label>Data de aprovação:</label>
-            </div>
-            <div>
-                <input type="text" />
-         
-                <input type="text" />
-        
-                <input type="text" />
-            </div>
-            <div>
-                <Button tipo={"Responsavel"} text={"Continuar"} />
-            </div>
+            <form action="" method="post">
+                <div>
+                    <label>Data de ínicio:</label>
+                    <label>Data de término:</label>
+                    <label>Data de aprovação:</label>
+                </div>
+                <div>
+                    <input type="text" />
+            
+                    <input type="text" />
+            
+                    <input type="text" />
+                </div>
+                <div>
+                    <Button tipo={"Responsavel"} text={"Continuar"} />
+                </div>
+            </form>
         </Box>
         </>    
     );
