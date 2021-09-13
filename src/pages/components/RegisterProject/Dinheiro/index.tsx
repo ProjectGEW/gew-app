7da3254590e615ed20432e54c6f911aabecfb389
Thomas Maffe <thomasmaffezzolli8@gmail.com>
2021-09-13 19:23:27 -0300

    C:/Program Files/Git/

--- src/pages/components/RegisterProject/Dinheiro/index.tsx
+++ src/pages/components/RegisterProject/Dinheiro/index.tsx
@@ -7,38 +7,36 @@ const Dinheiro: React.FC = () => {
     const [teste, setTeste] = useState<JSX.Element[]>([]);
     return (
         <>
-        <BoxDinheiro>
-            <form action="" method="post">
-                <Table>
-                    <div id="first-table">
-                        <h1>Despesas (desembolsos)</h1>
-                        <h1>Esforço</h1>
-                        <h1>Valor (R$)</h1>
-                    </div>
-                    <div id="first-scroll">
-                        {teste.map(teste => teste)}
-                        <div onClick={() => { setTeste([...teste, <RowDinheiro />]) }}>TESTE</div>
-                        <RowDinheiro />
-                        <Total>
-                            <h2>TOTAL:</h2>
-                            <input id="totalEsforco" type="text" defaultValue="1500h" className="alinhar" />
-                            <input id="totalValor" type="text" defaultValue="40.000,00" className="alinhar" />
-                        </Total>
-                    </div>
-                </Table>
-                <Table>
-                    <div id="second-table">
-                        <h1>Centro de Custo</h1>
-                        <h1>Responsável</h1>
-                        <h1>Percentual</h1>
-                        <h1>Valor (R$)</h1>
-                    </div>
-                    <div id="second-scroll">
-                    </div>
-                </Table>
-                <Button tipo={"Dinheiro"} text={"Continuar"} />
-            </form>
-        </BoxDinheiro>
+            <BoxDinheiro>
+                <form action="" method="post">
+                    <Table>
+                        <div id="first-table">
+                            <h1>Despesas (desembolsos)</h1>
+                            <h1>Esforço</h1>
+                            <h1>Valor (R$)</h1>
+                        </div>
+                        <div id="first-scroll">
+                            {teste.map(teste => teste)}
+                            <Total>
+                                <h2>TOTAL:</h2>
+                                <input id="totalEsforco" type="text" defaultValue="1500h" className="alinhar" />
+                                <input id="totalValor" type="text" defaultValue="40.000,00" className="alinhar" />
+                            </Total>
+                        </div>
+                    </Table>
+                    <Table>
+                        <div id="second-table">
+                            <h1>Centro de Custo</h1>
+                            <h1>Responsável</h1>
+                            <h1>Percentual</h1>
+                            <h1>Valor (R$)</h1>
+                        </div>
+                        <div id="second-scroll">
+                        </div>
+                    </Table>
+                    <Button tipo={"Dinheiro"} text={"Continuar"} />
+                </form>
+            </BoxDinheiro>
         </>
     );
 };
