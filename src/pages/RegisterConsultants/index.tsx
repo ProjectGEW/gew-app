be97b1c186dbd4b29fcc3ef4e6f0b87fc2e607e1
Roberta Baseggio <roberta_polez@estudante.sc.senai.br>
2021-09-13 16:44:48 -0300

    Elementação da tela de cadastro de consultores

--- src/pages/RegisterConsultants/index.tsx
+++ src/pages/RegisterConsultants/index.tsx
@@ -7,8 +7,7 @@ import MenuRight from '../components/MenuRight';
 import { ContIcons } from '../components/MenuRight/styles';
 import { AiOutlineCaretDown } from 'react-icons/ai';
 
-import { Container } from './styles';
-
+import { Container, InputGrid, Box } from './styles';
 
 const RegisterConsultants: React.FC = () => {
 
@@ -18,10 +17,28 @@ const RegisterConsultants: React.FC = () => {
             <MenuLeft />
         
             <Container>
-                <div>
+                <header>
                     <h1>Cadastrar consultor</h1>
                     <AiOutlineCaretDown />
-                </div>
+                </header>
+                <InputGrid>
+                    <Box>
+                        <li className="one">
+                            <label>Nome:</label>
+                            <input type="text" />
+                        </li>
+                    </Box>
+                    <Box className='two'>
+                        <li>
+                            <label>Nome:</label>
+                            <input type="text" />
+                        </li>
+                        <li>
+                            <label>Nome:</label>
+                            <input type="text" />
+                        </li>
+                    </Box>
+                </InputGrid>
             </Container>
 
             <MenuRight>
