2d3f7bed07c9eb4891db4b047a129043b57eb290
Roberta Baseggio <roberta_polez@estudante.sc.senai.br>
2021-09-13 14:27:00 -0300

    Criação da tela de cadastro de consultores

--- src/pages/RegisterConsultants/index.tsx
+++ src/pages/RegisterConsultants/index.tsx
@@ -5,16 +5,24 @@ import Navbar from '../components/Navbar';
 import MenuRight from '../components/MenuRight';
 
 import { ContIcons } from '../components/MenuRight/styles';
+import { AiOutlineCaretDown } from 'react-icons/ai';
 
 import { Container } from './styles';
 
 
-const Projects: React.FC = () => {
+const RegisterConsultants: React.FC = () => {
 
     return (
         <>
             <Navbar />
             <MenuLeft />
+        
+            <Container>
+                <div>
+                    <h1>Cadastrar consultor</h1>
+                    <AiOutlineCaretDown />
+                </div>
+            </Container>
 
             <MenuRight>
                 <ContIcons />
@@ -23,4 +31,4 @@ const Projects: React.FC = () => {
         );
 };
 
-export default Projects;
+export default RegisterConsultants;
--- src/pages/RegisterConsultants/styles.ts
+++ src/pages/RegisterConsultants/styles.ts
@@ -3,16 +3,37 @@ import styled from 'styled-components';
 export const Container = styled.div`
     position: absolute;
     top: 14vh;
-    left: 12vw;
-    width: 62vw;
+    left: 33.5vw;
+    width: 32vw;
     height: 80vh;
 
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
 
     background-color: #fff;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
     border-radius: 0.8vh;
+
+    div {
+        width: 100%;
+        height: 8vh;
+        background-color: #00579D;
+        border-radius: 1vh 1vh 0 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 3vh 0 3vh;
+
+        h1 {
+            font-size: 3.7vh;
+            font-weight: bold;
+            color: #fff;
+        }
+
+        svg {
+            width: 3.6vh;
+            height: auto;
+            color: #fff;
+            margin-top: 0.4vh;
+        }
+    }
 `;
