947fda33e277cfee083bd30cd8b8cd52602860f5
ThiagoPetry <thiagokeiserpetry@gmail.com>
2021-09-13 16:14:51 -0300

    Atualização do app

--- src/pages/components/RegisterProject/Dinheiro/Row/RowCC/index.tsx
+++ src/pages/components/RegisterProject/Dinheiro/Row/RowCC/index.tsx
@@ -4,10 +4,10 @@ import { Linha } from "../../styles";
 const RowCcPagantes: React.FC = () => {
   return(
     <Linha>
-        <input type="text" value="092765143111" />
-        <input type="text" value="André Borges Lima" />
-        <input type="text" value="50%" className="alinhar" />
-        <input type="text" value="23.500,00" className="alinhar" />
+        <input type="text" defaultValue="092765143111" />
+        <input type="text" defaultValue="André Borges Lima" />
+        <input type="text" defaultValue="50%" className="alinhar" />
+        <input type="text" defaultValue="23.500,00" className="alinhar" />
     </Linha>
   );
 }
