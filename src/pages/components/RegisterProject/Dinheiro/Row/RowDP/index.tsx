166341bf63fff8e002b948f73c1943f1f14c3d2e
Thomas Maffe <thomasmaffezzolli8@gmail.com>
2021-09-13 19:21:26 -0300

    Adição da interface com a propriedade de numero para mudar o name dos inputs

--- src/pages/components/RegisterProject/Dinheiro/Row/RowDP/index.tsx
+++ src/pages/components/RegisterProject/Dinheiro/Row/RowDP/index.tsx
@@ -1,12 +1,16 @@
 import React from "react"
 import { Linha } from "../../styles";
 
-const RowDespesas: React.FC = () => {
-  return(
+interface Iteste {
+  number: number;
+}
+
+const RowDespesas: React.FC<Iteste> = (number) => {
+  return (
     <Linha>
-        <input type="text" id="despesa1" placeholder="Java"/>
-        <input type="text" id="esforco1" placeholder="100"/>
-        <input type="text" id="valor1" placeholder="20.000,00" className="alinhar" />
+      <input type="text" id="despesa1" placeholder="Java" />
+      <input type="text" id="esforco1" placeholder="100" />
+      <input type="text" id="valor1" placeholder="20.000,00" className="alinhar" />
     </Linha>
   );
 }
