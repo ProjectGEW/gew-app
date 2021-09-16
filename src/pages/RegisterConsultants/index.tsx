d9bdc5152beee4fa86ea041e5672ee25053a52d8
Cauã <cauakathdev@gmail.com>
2021-09-16 09:40:00 -0300

    Envio de informações dos campos para api

--- src/pages/RegisterConsultants/index.tsx
+++ src/pages/RegisterConsultants/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import MenuLeft from '../components/MenuLeft';
 import Navbar from '../components/Navbar';
@@ -10,27 +10,54 @@ import { BsSearch, BsFillPersonLinesFill } from 'react-icons/bs';
 import { GoPencil } from 'react-icons/go';
 
 import { ConsultantData, Container, PricePerHour, SideContainer, SupplierData, UserData } from './styles';
-import Button from '../components/Button';
 import Footer from '../components/Footer';
-import internal from 'stream';
 import { RiLockPasswordLine } from 'react-icons/ri';
 
-interface CadConsultants {
-    infoConsultants: {
-            nome: string;
-            email: string;
-            cpf: number;
-            telefone: number;
-            precoPorHora: number;
-    };
+import api from "../../service/api";
 
-    infoSupplier: {
-        nome: string;
-        email: string;
-    }
+interface CadConsultants {
+    infoConsultor: Consultor;
+    infoFornecedor: Fornecedor;
+}
+
+interface Fornecedor {
+    nome: string;
+    email: string;
+}
+
+interface Consultor {
+    numero_cracha: number;
+    nome: string;
+    email: string;
+    senha: string;
+    cpf: number;
+    telefone: string;
+    valor_hora: number;
 }
 
 const RegisterConsultants: React.FC = () => {
+    const setConsultorInfos = useCallback( async () => {
+        const numero_cracha = parseInt((document.getElementById("numero_cracha") as HTMLInputElement).value);
+        const nome = (document.getElementById("nome") as HTMLInputElement).value;
+        const email = (document.getElementById("email") as HTMLInputElement).value;
+        const senha = (document.getElementById("senha") as HTMLInputElement).value;
+        const cpf = parseInt((document.getElementById("cpf") as HTMLInputElement).value);
+        const telefone = (document.getElementById("telefone") as HTMLInputElement).value;
+        const valor_hora = parseFloat((document.getElementById("valor_hora") as HTMLInputElement).value);
+
+        const consultor: Consultor = {
+            numero_cracha: numero_cracha,
+            nome: nome,
+            email: email,
+            senha: senha,
+            cpf: cpf,
+            telefone: telefone,
+            valor_hora: valor_hora
+        };
+
+        await api.post(`funcionarios/consultor`, consultor);
+    }, []);
+
     return (
         <>
             <Navbar />
@@ -47,22 +74,22 @@ const RegisterConsultants: React.FC = () => {
 
                         <div className="box1">
                             <label>Nome:</label>
-                            <input type='text' />
+                            <input type='text' id="nome" />
                             <GoPencil />
                         </div>
                         <div className="box3">
                             <label>CPF:</label>
-                            <input type='text' placeholder='---.---.--- - --' />
+                            <input type='text' id="cpf" placeholder='---.---.--- - --' />
                             <BsFillPersonLinesFill />
                         </div>
                         <div className="box4">
                             <label>Telefone:</label>
-                            <input type='text' placeholder='(--) ----- - ----' />
+                            <input type='text' id="telefone" placeholder='(..).....-....' />
                             <AiOutlinePhone />
                         </div>
                         <div className="box3">
                             <label>N° do crachá:</label>
-                            <input type='text' placeholder='------' />
+                            <input type='text' id="numero_cracha" placeholder='------' />
                             <BsFillPersonLinesFill />
                         </div>
                     </ConsultantData>
@@ -70,7 +97,7 @@ const RegisterConsultants: React.FC = () => {
                         <h1>Preço por hora</h1>
                         <div>
                             <label>Valor horista:</label>
-                            <input type='text' placeholder="R$  --,--" />
+                            <input type='text' id="valor_hora" placeholder="R$  --,--" />
                         </div>
                     </PricePerHour>
                 </SideContainer>
@@ -80,31 +107,28 @@ const RegisterConsultants: React.FC = () => {
 
                         <div className="box1">
                             <label>Email do usuário:</label>
-                            <input type='text' />
+                            <input type='text' id="email" />
                             <AiOutlineMail />
                         </div>
                         <div className="box1">
                             <label>Senha:</label>
-                            <input type='password' placeholder="********" />
+                            <input type='password' id="senha" placeholder="********" />
                             <RiLockPasswordLine />
                         </div>
                     </UserData>
                     <SupplierData>
-                        <h1>Dados do fornecedor</h1>
+                        <h1>Fornecedor</h1>
 
-                        <div className="box1">
-                            <label>E-mail do fornecedor:</label>
-                            <input type='text' />
-                            <AiOutlineMail />
-                        </div>
                         <div className="box5">
-                            <label>Fornecedor:</label>
+                            <label>Nome:</label>
                             <input type='text'  />
                             <BsSearch />
                         </div>
                     </SupplierData>
 
-                    <Button tipo={"register_consultants"} text={"Cadastrar"} />
+                    <button id="enviarDados" onClick={setConsultorInfos}>
+                        Cadastrar
+                    </button>
                     
                 </SideContainer>
                 <Footer tipo={"register_consultants"}>
