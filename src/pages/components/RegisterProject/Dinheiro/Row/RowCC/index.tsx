a2931deca35f2bcbcd3ae484d70c6ffa89b175ae
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-09-22 15:58:21 -0300

    Adição do estilo próprio

--- src/pages/components/RegisterProject/Dinheiro/Row/RowCC/index.tsx
+++ src/pages/components/RegisterProject/Dinheiro/Row/RowCC/index.tsx
@@ -1,15 +1,55 @@
 import React from "react"
-import { Linha } from "../../styles";
+import styled from "styled-components";
 
 interface Iteste {
   number: number;
 }
 
+const Linha = styled.div`
+  display: block;
+  width: 100%;
+  transition: all 0.2s;
+
+  input {
+      display: block;
+      height: 5vh;
+
+      border-radius: 0.4vh;
+      border: 0px;
+      padding: 1vh;
+
+      background-color: rgb(181, 181, 181, 0.6);
+      box-shadow: inset 0.1vh 0.1vh 0.1vh rgb(52, 52, 52, 0.25);
+      color: #5E5E5E;
+
+      
+      &:nth-child(1) {
+          width: 12vw;
+          margin-left: 0vw;
+      }
+
+      &:nth-child(2) {
+          width: 16vw;
+          margin-left: 0vw;
+      }
+
+      &:nth-child(3) {
+          width: 6vw;
+          margin-right: 0vw;
+      }
+
+      &:nth-child(4) {
+          width: 10vw;
+          margin-right: 0vw;
+      }
+                
+  }
+`;
+
 const RowCcPagantes: React.FC<Iteste> = (number) => {
 
   const conCatCentroDeCusto = `centro${number.number}`;
   const conCatValor = `valorC${number.number}`;
-
 
   return (
     <Linha id={`C${number.number}`}>
