1a19b741450c2c55d6c13ac1a1026802ddae1c12
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-09-22 15:57:50 -0300

    Atualização das tabelas

--- src/pages/components/RegisterProject/Datas/styles.ts
+++ src/pages/components/RegisterProject/Datas/styles.ts
@@ -18,13 +18,12 @@ export const BoxDatas = styled.div<FormDataProps>`
     .spanDatas {
         width: 100%;
         height: 10vh;
+        margin-top: 2vh;
 
         display: flex;
         align-items: center;
         flex-direction: column;
-        justify-content: center;
-
-        
+        justify-content: center;        
     }
 
     .divDatas {
@@ -37,8 +36,7 @@ export const BoxDatas = styled.div<FormDataProps>`
         label {
             width: 13vw;
             color: #00579D;
-            font-size: 2.6vh;
-            font-weight: bold;
+            font-size: 2.8vh;
 
             margin-bottom: 0.5vh;
             text-align: left;
@@ -53,6 +51,9 @@ export const BoxDatas = styled.div<FormDataProps>`
             height: 5vh;
             margin-bottom: 0.5vh;
 
+            font-size: 2.2vh;
+            text-align: center;
+
             border-radius: 0.4vh;
             border: 0px;
             padding: 1vh;
@@ -64,22 +65,19 @@ export const BoxDatas = styled.div<FormDataProps>`
 
         #data_de_inicio {
             ${props => props.hasErrorInicio && css`
-                border: 0.2vh solid #c53030;
-                box-shadow: inset 0.1vh 0.1vh 0.1vh rgb(255, 0, 0, 0.25);
+                border: 0.25vh solid rgb(255, 0, 0, 0.8);
             `}
         }
 
         #data_de_termino {
             ${props => props.hasErrorFim && css`
-                border: 0.2vh solid #c53030;
-                box-shadow: inset 0.1vh 0.1vh 0.1vh rgb(255, 0, 0, 0.25);
+                border: 0.25vh solid rgb(255, 0, 0, 0.8);
             `}
         }
 
         #data_de_aprovacao {
             ${props => props.hasErrorAprovacao && css`
-                border: 0.2vh solid #c53030;
-                box-shadow: inset 0.1vh 0.1vh 0.1vh rgb(255, 0, 0, 0.25);
+                border: 0.25vh solid rgb(255, 0, 0, 0.8);
             `}
         }
     }
@@ -91,24 +89,28 @@ export const BoxDatas = styled.div<FormDataProps>`
 
         border: 0;
         border-radius: 0.8vh;
-        box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+        box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
 
         .react-calendar__navigation {
-            height: 30px;
-            border-bottom: 0.1vh solid rgba(60, 64, 67, 0.15);            
-            margin-bottom: 0.1vh;
+            height: 4vh;
+            background: #00579D;
+            border-bottom: 0vh solid rgba(60, 64, 67, 0.15);            
+            margin-bottom: 0vh;
             margin-top: 0vh;
+            border-radius: 0.8vh 0.8vh 0 0;
         }
 
         .react-calendar__month-view__weekdays {
             font-size: 1.8vh;
+            background: #00579D;
+            color: #fff;
         }
 
         .react-calendar__navigation button {
             font-size: 2.4vh;
             text-transform: uppercase;
             font-weight: bold;
-            color: #00579D;
+            color: #fff;
 
             &:nth-child(1), &:nth-child(2), &:nth-child(4), &:nth-child(5) {
                 font-size: 3vh;
@@ -119,25 +121,26 @@ export const BoxDatas = styled.div<FormDataProps>`
             padding: 1vh;
             padding-top: 1.5vh;
             font-weight: bold;
+            border-radius: 0.4vh;
+            box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
+            color: #222;
         }
 
         .react-calendar__month-view__days__day--weekend {
-            color: #54b4ff;
-            font-weight: 400;
+            color: #36a6ff;
         }
 
         .react-calendar__month-view__days__day--neighboringMonth {
-            color: #757575;
-            font-weight: 400;
+            color: #999;
         }
 
         .react-calendar__tile--now {
-            background: #85ff89;
+            background: #78ff7c;
         }
 
         .react-calendar__tile--now:enabled:hover,
         .react-calendar__tile--now:enabled:focus {
-            background: #adffb0;
+            background: #96ff9a;
         }
 
         .react-calendar__tile--active {
@@ -148,6 +151,16 @@ export const BoxDatas = styled.div<FormDataProps>`
         .react-calendar__tile--active:enabled:hover,
         .react-calendar__tile--active:enabled:focus {
             background: #006abf;
+        }
+
+        .react-calendar__tile:hover {
+            background-color: #c2e4ff;
+        }
+
+        .react-calendar__navigation button:enabled:hover,
+        .react-calendar__navigation button:enabled:focus {
+            border-radius: 0.8vh 0.8vh 0 0;
+            background-color: #004f8f;
         }
     }
 `;
