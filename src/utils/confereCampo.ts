b11dddcdd3bcbfc1f90a78c1c0ef4b3b8897439c
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-09-24 19:39:05 -0300

    Atualização das mensagens de erro

--- src/utils/confereCampo.ts
+++ src/utils/confereCampo.ts
@@ -12,9 +12,12 @@ export default function analisaCampo(idElement: string, textResponse: string, id
 export function analisaCampoLinhasdDespesas(listaDespesa: number) {
   let confirm = 0;
   for (let i = 1; i <= listaDespesa; i++) {
-    confirm += analisaCampo(`despesa${i}`, "Por favor insira a Despesa", `despesaResponse${i}`);
-    confirm += analisaCampo(`esforco${i}`, "Por favor insira o Esforco", `esforcoResponse${i}`);
-    confirm += analisaCampo(`valor${i}`, "Por favor insira o Valor", `valorResponse${i}`);
+    /*confirm += analisaCampo(`despesa${i}`, "Por favor insira a Despesa", `despesaResponse${i}`);
+    confirm += analisaCampo(`esforco${i}`, "Por favor insira o Esforço", `esforcoResponse${i}`);
+    confirm += analisaCampo(`valor${i}`, "Por favor insira o Valor", `valorResponse${i}`);*/
+    confirm += analisaCampo(`despesa${i}`, "Insira a Despesa", `despesaResponse${i}`);
+    confirm += analisaCampo(`esforco${i}`, "Insira o Esforço", `esforcoResponse${i}`);
+    confirm += analisaCampo(`valor${i}`, "Insira o Valor", `valorResponse${i}`);
   }
   if (confirm < listaDespesa * 3) {
     return 0; 
@@ -25,8 +28,10 @@ export function analisaCampoLinhasdDespesas(listaDespesa: number) {
 export function analisaCampoLinhasdCcPagantes(listaCcPagantes: number) {
   let confirm = 0;
   for (let i = 1; i <= listaCcPagantes; i++) {
-    confirm += analisaCampo(`centro${i}`, "Por favor insira a CcPagante", `ccPagantesResponse${i}`);
-    confirm += analisaCampo(`valorC${i}`, "Por favor insira o Valor", `ccValorResponse${i}`);
+    /*confirm += analisaCampo(`centro${i}`, "Por favor insira a CcPagante", `ccPagantesResponse${i}`);
+    confirm += analisaCampo(`valorC${i}`, "Por favor insira o Valor", `ccValorResponse${i}`);*/
+    confirm += analisaCampo(`centro${i}`, "Insira a CcPagante", `ccPagantesResponse${i}`);
+    confirm += analisaCampo(`valorC${i}`, "Insira o Valor", `ccValorResponse${i}`);
   }
   if (confirm < listaCcPagantes * 2) {
     return 0; 
