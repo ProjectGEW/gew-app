a084553b8b67bd17acf4aaff3966ef335f4c68f7
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-10-09 12:15:52 -0300

    Remoção de avisos e códigos não utilizados

--- src/pages/Consultants/ProjectsList/index.tsx
+++ src/pages/Consultants/ProjectsList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import MenuLeft from '../../components/MenuLeft';
 import Navbar from '../../components/Navbar';
