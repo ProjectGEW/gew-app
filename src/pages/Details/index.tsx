efb9c855db78264e3146954b64c7e41daf3134e9
ThiagoPetry <thiagokeiserpetry@gmail.com>
2021-10-22 11:39:03 -0300

    Alteração das rotas de conexão com a api

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { Inputs, ContainerDesc, ContainerInfos, ContainerSection, ContainerTittles, Tittle, Container,
