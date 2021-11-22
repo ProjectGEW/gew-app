038dae69d4dd7a1e8dec749052719bc917c6a16e
victorstassun <victorstassun@gmail.com>
2021-11-22 08:15:33 -0300

    Conclusão e ligação da tela de login com o Back-End

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -23,21 +23,21 @@ import NewLogin from '../pages/NewLogin';
 const Routes: React.FC = () => (
     <Switch>
         {/* Adicionar isPrivate no final de cada <Route> -> <Route ... isPrivate /> */}
-        <Route path="/" exact component={Login}/>
-        <Route path="/home" component={Menu} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/register_projects" component={RegisterProjects} />
-        <Route path="/edit_projects" component={EditProjects} />
-        <Route path="/register_consultants" component={RegisterConsultants} />
-        <Route path="/settings" component={Settings} />
-        <Route path="/dashboard/:id" component={Dashboard} />
-        <Route path="/details/:numeroDoProjeto" component={Details} />
-        <Route path="/consultants/view_projects" component={ProjectsList} />
-        <Route path="/consultants/:numeroDoProjeto" component={ConsultantList} />
-        <Route path="/consultants/profile/:numeroCracha" component={ConsultantProfile} />
-        <Route path="/edit/:nm" component={Edit} />
+        <Route path="/oldlogin" exact component={Login}/>
+        <Route path="/home" component={Menu} isPrivate/>
+        <Route path="/projects" component={Projects} isPrivate/>
+        <Route path="/register_projects" component={RegisterProjects} isPrivate/>
+        <Route path="/edit_projects" component={EditProjects} isPrivate/>
+        <Route path="/register_consultants" component={RegisterConsultants} isPrivate/>
+        <Route path="/settings" component={Settings} isPrivate/>
+        <Route path="/dashboard/:id" component={Dashboard} isPrivate/>
+        <Route path="/details/:numeroDoProjeto" component={Details} isPrivate/>
+        <Route path="/consultants/view_projects" component={ProjectsList} isPrivate/>
+        <Route path="/consultants/:numeroDoProjeto" component={ConsultantList} isPrivate/>
+        <Route path="/consultants/profile/:numeroCracha" component={ConsultantProfile} isPrivate/>
+        <Route path="/edit/:nm" component={Edit} isPrivate/>
         {/* Área para testes */}
-        <Route path="/newlogin" component={NewLogin} />
+        <Route path="/" exact component={NewLogin}/>
         <Route path="/test" component={Test} />
         <Route path="/test2" component={Test2} />
     </Switch>
