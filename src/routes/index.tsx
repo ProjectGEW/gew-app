340130ada11fd13dad4636d8d4a67b3e1bca2a3f
thomas <thomasmaffezzolli8@gmail.com>
2021-12-06 08:33:32 -0300

    Reorganização da arquivo com as novas rotas
    Adição de comentários para organização e as novas rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,6 @@ import Route from './Route';
 import Login from '../pages/Login';
 import Menu from '../pages/Menu';
 import RegisterConsultants from '../pages/RegisterConsultants';
-import RC from '../pages/RegisterConsultants/index2';
 import Projects from '../pages/Projects';
 import CadastroProjeto from '../pages/RegisterProjects/index2';
 import EditProjects from '../pages/EditProjects';
@@ -21,29 +20,43 @@ import ConsultantProfile from '../pages/Consultants/ConsultantProfile';
 import Edit from '../pages/EditProjects/Edit/index2';
 import Consultor from '../pages/Consultor';
 import NewLogin from '../pages/NewLogin';
+import Fornecedor from '../pages/Fornecedor';
+import Gestor from '../pages/Gestor';
 
 const Routes: React.FC = () => (
-    <Switch>
-        {/* Adicionar isPrivate no final de cada <Route> -> <Route ... isPrivate /> */}
-        <Route path="/" exact component={NewLogin}/>
-        <Route path="/home" component={Menu} />
-        <Route path="/homeConsultor" component={Consultor} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/register_projects" component={CadastroProjeto} />
-        <Route path="/edit_projects" component={EditProjects} />
-        <Route path="/register_consultants" component={RegisterConsultants} />
-        <Route path="/settings" component={Settings} />
-        <Route path="/dashboard/:id" component={Dashboard} />
-        <Route path="/details/:numeroDoProjeto" component={Details} />
-        <Route path="/consultants/view_projects" component={ProjectsList} />
-        <Route path="/consultants/:numeroDoProjeto" component={ConsultantList} />
-        <Route path="/consultants/profile/:numeroCracha" component={ConsultantProfile} />
-        <Route path="/edit/:nm" component={Edit} />
-        {/* Área para testes */}
-        <Route path="/oldlogin" component={Login}/>
-        <Route path="/test" component={Test} />
-        <Route path="/test2" component={Test2} />
-    </Switch>
+  <Switch>
+    {/* Adicionar isPrivate no final de cada <Route> -> <Route ... isPrivate /> */}
+    {/* Rotas gerais para todas as personas */}
+    <Route path="/" exact component={NewLogin} isPrivate />
+    <Route path="/settings" component={Settings} isPrivate/>
+
+    {/* Rotas para Gerente */}
+    <Route path="/home" component={Menu} isPrivate />
+    <Route path="/projects" component={Projects} isPrivate/>
+    <Route path="/register_projects" component={CadastroProjeto} isPrivate/>
+    <Route path="/edit_projects" component={EditProjects} isPrivate/>
+    <Route path="/register_consultants" component={RegisterConsultants} isPrivate/>
+    <Route path="/dashboard/:id" component={Dashboard} isPrivate/>
+    <Route path="/details/:numeroDoProjeto" component={Details} isPrivate/>
+    <Route path="/consultants/view_projects" component={ProjectsList} isPrivate/>
+    <Route path="/consultants/:numeroDoProjeto" component={ConsultantList} isPrivate/>
+    <Route path="/consultants/profile/:numeroCracha" component={ConsultantProfile} isPrivate/>
+    <Route path="/edit/:nm" component={Edit} isPrivate/>
+
+    {/* Rotas para o gestor */}
+    <Route path="/gestor" component={Gestor} isPrivate/>
+
+    {/* Rotas para o fornecedor */}
+    <Route path="/fornecedor" component={Fornecedor} isPrivate/>
+    
+    {/* Rotas para o consultor */}
+    <Route path="/consultor" component={Consultor} isPrivate/>
+
+    {/* Área para testes */}
+    <Route path="/oldlogin" component={Login}/>
+    <Route path="/test" component={Test} isPrivate />
+    <Route path="/test2" component={Test2} />
+  </Switch>
 );
 
 export default Routes;
