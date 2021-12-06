2a6b7887a06c2ebef76e577de08fe7de82e74427
thomas <thomasmaffezzolli8@gmail.com>
2021-12-06 08:30:55 -0300

    Adição do esquema cru do consultor
    Adição do index para futuro desenvolvimento

--- src/pages/Consultor/index.tsx
+++ src/pages/Consultor/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import MenuLeft from '../components/MenuLeft';
+import Navbar from '../components/Navbar';
+import { ContainerProject } from '../Projects/styles';
+import { Container  } from './styles';
 
 const Consultor: React.FC = () => {
   return (
     <>
-      <h1>Jhones massa</h1>
+       <Navbar />
+       <MenuLeft />
+       <Container >
+        <ContainerProject>
+          <h1>Consultor</h1>
+        </ContainerProject>
+       </Container>
     </>
   )
 }
