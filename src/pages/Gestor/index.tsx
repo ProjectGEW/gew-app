b399615d802edcaea5638c12b0edd2d25fde733e
thomas <thomasmaffezzolli8@gmail.com>
2021-12-06 08:31:53 -0300

    Adição do esquema cru do gestor
    Adição do index para futuro desenvolvimento

---
+++ src/pages/Gestor/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import MenuLeft from '../components/MenuLeft';
+import Navbar from '../components/Navbar';
+import { ContainerProject } from '../Projects/styles';
+import { Container  } from './styles';
+
+const Gestor: React.FC = () => {
+  return (
+    <>
+       <Navbar />
+       <MenuLeft />
+       <Container >
+        <ContainerProject>
+          <h1>Gestor</h1>
+        </ContainerProject>
+       </Container>
+    </>
+  )
+}
+
+export default Gestor;
