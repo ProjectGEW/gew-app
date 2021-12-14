baf630e557ae4c2c3600d29ae0f55ac14f3c3086
Thomas Maffe <thomasmaffezzolli8@gmail.com>
2021-12-13 21:21:09 -0300

    ..

--- src/components/CardProjeto/CardEsqueleto/index.tsx
+++ src/components/CardProjeto/CardEsqueleto/index.tsx
@@ -7,8 +7,8 @@ const CardProject: React.FC = () => {
   return (
     <>
       <Card>
-        <CardStatus/>
-        <CardBox/>
+        <CardStatus />
+        <CardBox />
       </Card>
     </>
   );
