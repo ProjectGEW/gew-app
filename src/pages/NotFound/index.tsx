66cf4df25bfeb14365bf9f037eaf0f70caaa1af7
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2022-01-23 14:32:25 -0300

    Página para rotas não encontradas

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import intl from 'react-intl-universal';
 
 import Navbar from '../../components/Navbar';
 
 import { Container } from './styles';
 
+const locales = {
+  'pt-BR': require('../../language/pt-BR.json'),
+  'en-US': require('../../language/en-US.json'),
+  'es': require('../../language/es.json'),
+  'fr-FR': require('../../language/fr-FR.json'),
+};
+
 const Projects: React.FC = () => {  
+  const [language] = useState(() => {
+    let languageStorage = localStorage.getItem('Language');
+
+    if (languageStorage) {
+      let languageObject = JSON.parse(languageStorage);
+      return languageObject;
+    }
+  });
+
+  intl.init({
+    currentLocale: language.code,
+    locales
+  });
+
   return (
     <>
     <Navbar/>
     <Container>
       <div>
-        <h1>Página não encontrada!</h1>
+        <h1>{intl.get('textos_em_geral.notfound')}</h1>
       </div>
     </Container>
     </>
