76393d4dc76625de7feb1dacae0fc5802500ef94
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2022-01-23 14:37:43 -0300

    Adição do toast de info

--- src/hooks/SystemToasts.tsx
+++ src/hooks/SystemToasts.tsx
@@ -1,18 +1,24 @@
-/* PopUps de sucesso, aviso e erro */
+/* PopUps de sucesso, aviso, erro e info */
 
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 toast.configure();
 
+const tempo = 10000;
+
 export const successfulNotify = (mensagem: string) => {
-  toast.success(mensagem);
+  toast.success(mensagem, {autoClose: tempo});
 }
 
 export const errorfulNotify = (mensagem: string) => {
-  toast.error(mensagem);
+  toast.error(mensagem, {autoClose: tempo});
 }
 
 export const warnNotify = (mensagem: string) => {
-  toast.warn(mensagem);
+  toast.warn(mensagem, {autoClose: tempo});
+}
+
+export const infoNotify = (mensagem: string) => {
+  toast.info(mensagem, {autoClose: tempo});
 }
